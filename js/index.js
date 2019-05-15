700fe76125a3ff93540536a74586e489a94cad03
Artom Ryzhkov <temmyriz@gmail.com>
2019-05-15 17:06:23 +0300

    Done with animations

--- js/index.js
+++ js/index.js
@@ -1,3 +1,46 @@
 $(function () { 
-  console.log('ready');
+
+   // Active links scroll
+   $('.top-nav a').click(function(e) {
+    e.preventDefault();
+    var linkId = $(this).attr('href');
+    $('html, body').stop().animate({
+      scrollTop: $(linkId).offset().top
+    }, 700);
+    
+  });
+
+  $(window).scroll(function () { 
+    
+    var wScroll = $(this).scrollTop();
+    var menuPos = $('.top-nav').offset().top;
+
+    // fixin` menu
+    if(wScroll > menuPos) {
+      $('.top-nav').addClass('fixed');
+      
+    } else if (wScroll < $('.about').offset().top){
+      $('.top-nav').removeClass('fixed');
+    }
+    
+   
+    // Fade elements
+    if(wScroll > $('.our-team').offset().top - $(window).height() / 1.3) {
+      $('.our-team .member').each(function(i) {
+        setTimeout(function() {
+          $('.our-team .member').eq(i).addClass('showing');
+        }, 250 * (i+1));
+      });
+    }
+
+    if(wScroll > $('.posts-wrapper').offset().top - $(window).height() / 1.3) {
+      $('.posts-wrapper .post').each(function(i) {
+        setTimeout(function() {
+          $('.posts-wrapper .post').eq(i).addClass('active');
+        }, 250 * (i+1));
+      });
+      
+    }
+
+  });
 });
